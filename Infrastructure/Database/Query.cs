28c6563e3c3c63a5e7a9dc96962ecffbcd0c69d0
DaanAdr <parsa1815@outlook.com>
2025-04-07 14:53:35 +0200

    Added a simple Query class

--- Infrastructure/Database/Query.cs
+++ Infrastructure/Database/Query.cs
@@ -1,6 +1,19 @@
-﻿namespace graphql_api.Infrastructure.Database
+﻿using graphql_api.DataModels;
+
+namespace graphql_api.Infrastructure.Database
 {
     public class Query
     {
+        private readonly DirectorRepository _directorRepository;
+
+        public Query(DirectorRepository directorRepository)
+        {
+            _directorRepository = directorRepository;
+        }
+
+        public IQueryable<Director> GetDirectors()
+        {
+            return _directorRepository.GetDirectors();
+        }
     }
 }
