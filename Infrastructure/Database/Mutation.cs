0973050f76ff299be90e279f3598b5dbf1d2f0f1
DaanAdr <parsa1815@outlook.com>
2025-04-07 14:55:16 +0200

    Added a simple Mutation class

---
+++ Infrastructure/Database/Mutation.cs
@@ -0,0 +1,19 @@
+﻿using graphql_api.DataModels;
+
+namespace graphql_api.Infrastructure.Database
+{
+    public class Mutation
+    {
+        private readonly DirectorRepository _directorRepository;
+
+        public Mutation(DirectorRepository directorRepository)
+        {
+            _directorRepository = directorRepository;
+        }
+
+        public Director AddDirector(Director director)
+        {
+            return _directorRepository.AddDirector(director);
+        }
+    }
+}
