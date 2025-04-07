6d897fd371aba3779fc8c35d981834eac692faf2
DaanAdr <parsa1815@outlook.com>
2025-04-07 14:20:29 +0200

    Added DbContext

---
+++ Infrastructure/Database/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using graphql_api.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace graphql_api.Infrastructure.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Director> Director { get; set; }
+
+    }
+}
