a48b75b9333d232791d8376f16852fc10d81fbae
DaanAdr <parsa1815@outlook.com>
2025-04-17 10:23:41 +0200

    Authentication setup

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using System.Text;
 using graphql_api.Infrastructure.Database;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,12 +14,31 @@ builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
 
 builder.Services
     .AddGraphQLServer()
-    .AddGraphQLMoviesAPITypes();
-    //.AddMutationType<Mutation>();
+    .AddGraphQLMoviesAPITypes()
+    .AddAuthorization();
 
-var app = builder.Build();
+SymmetricSecurityKey signingKey = new SymmetricSecurityKey(
+    Encoding.UTF8.GetBytes("MySuperSecretKey"));
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters =
+            new TokenValidationParameters
+            {
+                ValidIssuer = "https://auth.chillicream.com",
+                ValidAudience = "https://graphql.chillicream.com",
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey
+            };
+    });
+
+WebApplication app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.MapGraphQL();
 app.Run();
