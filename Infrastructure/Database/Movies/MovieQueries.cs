539bbbc6600f7abe677c4457707bc0a5e9b9d25c
DaanAdr <parsa1815@outlook.com>
2025-04-23 13:26:34 +0200

    Added MovieType

--- Infrastructure/Database/Movies/MovieQueries.cs
+++ Infrastructure/Database/Movies/MovieQueries.cs
@@ -7,16 +7,26 @@ namespace graphql_api.Infrastructure.Database.Movies
     [QueryType]
     public static class MovieQueries
     {
-        public static async Task<List<Movie>> GetMoviesAsync(
+        public static async Task<List<MovieType>> GetMoviesAsync(
             AppDbContext databaseContext,
             ISelection selection)
         {
             // return await databaseContext.Movies.Select(selection).ToListAsync();
-            return await databaseContext.Movies
+            List<Movie> movies = await databaseContext.Movies
                 .Include(m => m.MovieActors)
                     .ThenInclude(ma => ma.Actor)
                 .Include(m => m.Director)
                 .ToListAsync();
+
+            List<MovieType> movieTypes = movies.Select(m => new MovieType()
+            {
+                Id = m.Id,
+                Title = m.Title,
+                Director = m.Director,
+                Actors = m.MovieActors.Select(ma => ma.Actor).ToList()
+            }).ToList();
+
+            return movieTypes;
         }
     }
 }
